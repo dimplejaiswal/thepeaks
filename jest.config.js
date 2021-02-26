5488811fc7183502deffef04d238ae374d67e8a3
Dimple Jaiswal <dimplejaiswal@Dimples-MacBook-Pro.local>
2021-02-27 00:00:04 +0530

    UPDATE: Background Img, DropDown, ReadMe

--- jest.config.js
+++ jest.config.js
@@ -37,6 +37,6 @@ module.exports = {
         '^.+\\.svg$': 'jest-svg-transformer',
         '^.+\\.scss$': 'jest-scss-transform',
         '^.+\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/_mocks_/fileMock.js',
+            '<rootDir>/__mocks__/fileMock.js',
     },
 };
